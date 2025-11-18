c1b9016c68d495fd4a947e4be7fbabb153e64b9e
brosnanwong <154567691+brosnanwong@users.noreply.github.com>
2025-11-18 15:48:16 +0800

    Add database configuration and connection setup

---
+++ tcg-backend/config/db.js
@@ -0,0 +1,21 @@
+// config/db.js
+require("dotenv").config();
+const mysql = require("mysql2");
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+});
+
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error("❌ DATABASE CONNECTION ERROR:", err);
+  } else {
+    console.log("✅ Connected to MySQL Database");
+    connection.release();
+  }
+});
+
+module.exports = db;
